26cb9480ef216827e54a924d22b7f0398f9d08ed
FajarAlfian <fajaralfian231@gmail.com>
2025-07-31 21:11:03 +0700

    fetchin api list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Search from './components/Search'
+import Loading from './components/Loading'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setLoading(true);
+    setErrorMessage('');
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      if (data.response === 'False') {
+        setErrorMessage(data.Error || 'An error occurred while fetching movies.');
+        setMovies([]);
+        return;
+      }
+      setMovies(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('An error occurred while fetching movies.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
 return (
   <main>
     <div className='pattern' />
@@ -11,11 +58,27 @@ return (
     <header>
       <img src='/hero.png' alt='Hero Banner' />
       <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassle</h1>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
     </header>
-    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    
+    <section className='all-movies'>
+      <h2 className='mt-[40px]'>All Movies</h2>
+      
+      {loading ?(
+        <Loading />
+      ) : errorMessage ? (
+        <p className='text-red-500'>{errorMessage}</p>
+      ) : (
+        <ul>
+          {movies.map(movie => (
+            <li className='text-white' key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      )}
+    </section>
     </div>
   </main>
-)
+  );
 }
 
 export default App
---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Loading.css'; 
+
+export const Loading = () => {
+  return (
+    <span class="loader"></span>
+  )
+}
+
+export default Loading;
